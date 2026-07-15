892cd137bde416de8f097f1bf1a60aec69ba94c4
qewr1324 <naiad.arts.studio@gmail.com>
2026-07-15 18:53:47 +0330

    fix(setting): fix package.json, ts.down.config,
    - Update package.json & add bun scripts
    - Add launch.json into './.vscode/~'
    - Fix main root '"main": "./dist/extension.cjs"' into ./package.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,14 +2,14 @@ import { execFileSync } from "node:child_process";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
+	entry: ["src/extension.ts"],
 	format: ["cjs"],
 	shims: false,
 	dts: false,
 	external: ["vscode"],
 	hooks(hooks) {
 		hooks.hookOnce("build:prepare", () => {
-			execFileSync("pnpm", ["run", "update"], { stdio: "inherit" });
+			execFileSync("bun", ["run", "update"], { stdio: "inherit" });
 		});
 	},
 });
