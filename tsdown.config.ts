697eedb35dbfb5f0de9d1a0a88347c2a1c61a974
qewr1324 <naiad.arts.studio@gmail.com>
2026-07-15 12:56:53 +0330

    fix(ts-module): rewrite package.json
    - Add type module into ./package.json
    - Update tsconfig.json
    - Update eslint.config.mjs to ~.js

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,19 +1,15 @@
-import { execFileSync } from 'node:child_process'
-import { defineConfig } from 'tsdown'
+import { execFileSync } from "node:child_process";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: [
-    'src/index.ts',
-  ],
-  format: ['cjs'],
-  shims: false,
-  dts: false,
-  external: [
-    'vscode',
-  ],
-  hooks(hooks) {
-    hooks.hookOnce('build:prepare', () => {
-      execFileSync('pnpm', ['run', 'update'], { stdio: 'inherit' })
-    })
-  },
-})
+	entry: ["src/index.ts"],
+	format: ["cjs"],
+	shims: false,
+	dts: false,
+	external: ["vscode"],
+	hooks(hooks) {
+		hooks.hookOnce("build:prepare", () => {
+			execFileSync("pnpm", ["run", "update"], { stdio: "inherit" });
+		});
+	},
+});
